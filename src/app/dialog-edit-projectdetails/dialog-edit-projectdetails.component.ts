578f07b2687169deb0ca3fe60385e66ebe8f8b04
Marcus-Haas <marcus-haas@hotmail.de>
2024-07-18 14:08:38 +0200

    update project-details

--- src/app/dialog-edit-projectdetails/dialog-edit-projectdetails.component.ts
+++ src/app/dialog-edit-projectdetails/dialog-edit-projectdetails.component.ts
@@ -1,16 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Project } from '../../models/project.class';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { FirebaseService } from '../firebase-service/firebase.service';
+import { User } from '../../models/user.class';
 
 @Component({
   selector: 'app-dialog-edit-projectdetails',
   standalone: true,
-  imports: [],
+  imports: [MatProgressBarModule, MatDialogModule, MatFormFieldModule, MatInputModule, MatButtonModule, FormsModule, MatSelectModule],
   templateUrl: './dialog-edit-projectdetails.component.html',
   styleUrl: './dialog-edit-projectdetails.component.scss'
 })
-export class DialogEditProjectdetailsComponent {
+export class DialogEditProjectdetailsComponent implements OnInit {
 
   loading = false;
   project: Project = new Project();
   projectId: string = '';
+  allUsers: User[] = [];
+
+  constructor(public dialogRef: MatDialogRef<DialogEditProjectdetailsComponent>, private service: FirebaseService) { }
+
+  ngOnInit(): void {
+    this.service.ReadData("user", this.allUsers);
+  }
+
+  saveProjectDetails() {
+    this.loading = true;
+    this.service.updateEvent(this.project, this.projectId)
+      .then(() => {
+        this.closeDialog();
+      });
+  }
+
+  closeDialog() {
+    setTimeout(() => {
+      this.loading = false;
+      this.dialogRef.close();
+    }, 1500);
+  }
 }
--- src/app/dialog-edit-revenue/dialog-edit-revenue.component.ts
+++ src/app/dialog-edit-revenue/dialog-edit-revenue.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { Project } from '../../models/project.class';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { FirebaseService } from '../firebase-service/firebase.service';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-dialog-edit-revenue',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, MatDialogModule, MatProgressBarModule, MatInputModule, FormsModule, MatButtonModule],
   templateUrl: './dialog-edit-revenue.component.html',
   styleUrl: './dialog-edit-revenue.component.scss'
 })
@@ -14,4 +21,22 @@ export class DialogEditRevenueComponent {
   project: Project = new Project();
   projectId: string = '';
 
+
+  constructor(public dialogRef: MatDialogRef<DialogEditRevenueComponent>, private service: FirebaseService) { }
+
+
+  saveRevenue() {
+    this.loading = true;
+    this.service.updateEvent(this.project, this.projectId)
+      .then(() => {
+        this.closeDialog();
+      });
+  }
+
+  closeDialog() {
+    setTimeout(() => {
+      this.loading = false;
+      this.dialogRef.close();
+    }, 1500);
+  }
 }
